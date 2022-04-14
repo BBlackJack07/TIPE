fa957fd45aafe674c9a65b18ff8bc8d7bef92575
BBlackJack07 <38078655+BBlackJack07@users.noreply.github.com>
2022-04-14 14:22:37 +0200

    Commenting

--- Crypto/rsa.ml
+++ Crypto/rsa.ml
@@ -5,7 +5,7 @@ let rec pgcd a b = match Z.(a = zero || b = zero) with
   | true -> if a = Z.zero then b else a
   | _    -> pgcd b (Z.rem a b)
 
-let euclide a b =
+let euclide a b = (* Algorithme d'Euclide étendu, pour calculer les coefficients de Bézout d'un couple d'entier *) 
   let u0,u1,v0,v1 = Z.(ref one, ref zero, ref zero, ref one) in
   let r0 = ref a and r1 = ref b in
   while !r1 <> Z.zero do
@@ -27,7 +27,8 @@ let rec modpow n e m = (* use fast exponentiation to compute n^(e) mod m*)
   | _ when Z.rem e two = Z.zero -> modpow Z.(rem (n*n) m) Z.(e/two) m
   | _ -> Z.(rem (n * (modpow Z.(rem (n*n) m) Z.(e/two) m)) m)
 
-let build_keys p q =
+let build_keys p q = (* p : Z.t, q : Z.t, p and q are supposed to be prime integers *)
+(* returned values are public and private rsa keys associated to p and q *)
   let phi = Z.((p-one)*(q-one)) in
   let n = Z.(p*q) in
   let e = ref Z.(phi-one) in
@@ -43,8 +44,8 @@ let build_keys p q =
   done;
   ({e= !e; n=n},{d= !d; n=n})
 
-let encrypt m pubkey = modpow m pubkey.e pubkey.n
+let encrypt m pubkey = (* encrypt Z.t int m using public key 'pubkey' *)
+  modpow m pubkey.e pubkey.n
 
-let decrypt c privkey = modpow c privkey.d privkey.n
-
-
+let decrypt c privkey = (* decrypt (or sign) Z.t int c using private key 'privkey' *)
+  modpow c privkey.d privkey.n
