f062ffd4422fcd5285db51da9cc7f33ee9ca7563
barb0k1 <nint.kar@gmail.com>
2022-06-23 11:17:32 +0200

    temps

--- Crypto/graphics.py
+++ Crypto/graphics.py
@@ -9,20 +9,37 @@ def readfile(path):
         for line in csvFile:
             X.append(int(line[0]))
             Y.append(float(line[1]))
-    return np.array(X),np.array(Y)
+    return X,Y
 
 naif = "naif.csv"
 miller = "miller_alone.csv"
 s1,t1 = readfile(naif)
+s1,t1=np.array(s1),np.array(t1)
 s2,t2 = readfile(miller)
 
+
+#%% Naif
 plt.close()
 
-#plt.plot(s1[1:],t1[1:],"g+")
 
-#a,b = np.polyfit(s1[1:],np.log(t1[1:]),1)
+plt.plot(s1[1:],t1[1:],"g+")
 
-#plt.plot(s1,np.exp(a*s1+b))
+a,b = np.polyfit(s1[1:],np.log(t1[1:]),1)
+l="t = {}*exp({} * nb_bits)".format(np.round(np.exp(b),11),np.round(a,4))
+
+plt.plot(s1,np.exp(a*s1+b),label=l)
+
+plt.title("Temps de génération d'un nombre premier en fonction de sa taille (en bits)")
+plt.xlabel("Nombre de bits")
+plt.ylabel("Temps (en secondes)")
+plt.legend()
+
+#%% Miller
+plt.close()
+
+s2 = np.array(s2[:212] + s2[213:232] + s2[233:])
+t2 = np.array(t2[:212] + t2[213:232] + t2[233:])
+
 
 plt.plot(s2,t2,"r+")
 c,d = np.polyfit(s2,t2,1)
@@ -31,3 +48,4 @@ plt.plot(s2,c*s2+d)
 plt.title("Temps de génération d'un nombre pseudo-premier en fonction de sa taille (en bits)")
 plt.xlabel("Nombre de bits")
 plt.ylabel("Temps (en secondes)")
+
